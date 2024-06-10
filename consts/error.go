041f62d07b3e43710eab57e5a665150bb9957b6e
Ezra Sitorus <ezra.indra@work.bri.co.id>
2024-06-10 19:59:51 +0700

    E: add ERR_SET_REDIS consts

--- consts/error.go
+++ consts/error.go
@@ -3,7 +3,7 @@ package consts
 const (
 	ERR_INVALID_CONFIG string = "invalid config"
 
-	TEST_PAGE_ERROR string = "TEST_PAGE_ERROR"
-
 	ERR_DATABSE string = "err database operation: "
+
+	ERR_SET_REDIS string = "err writing redis data: "
 )
