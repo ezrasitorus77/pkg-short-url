49119bbbbcad686509412de0041607d7a1a0e2bb
Ezra Sitorus <ezra.indra@work.bri.co.id>
2024-06-15 21:56:10 +0700

    E: add Alias column in RouteAccess struct for middleware validation

--- models/access.go
+++ models/access.go
@@ -6,6 +6,7 @@ type (
 	RouteAccess struct {
 		CreatedAt *time.Time `gorm:"column:created_at"`
 		APIKey    string     `gorm:"column:api_key"`
+		Alias     string     `gorm:"column:alias"`
 		Group     string     `gorm:"column:group"`
 		ID        int16      `gorm:"column:id;primary_key" json:"id"`
 	}
