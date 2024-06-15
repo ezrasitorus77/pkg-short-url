8d960697f5dbc999fc96cdf2143bb38d01b39493
Ezra Sitorus <ezra.indra@work.bri.co.id>
2024-06-15 14:16:48 +0700

    A: RouteAccess and MethodAccess structs for middleware

---
+++ models/access.go
@@ -0,0 +1,27 @@
+package models
+
+import "time"
+
+type (
+	RouteAccess struct {
+		CreatedAt *time.Time `gorm:"column:created_at"`
+		APIKey    string     `gorm:"column:api_key"`
+		Group     string     `gorm:"column:group"`
+		ID        int16      `gorm:"column:id;primary_key" json:"id"`
+	}
+
+	MethodAccess struct {
+		CreatedAt *time.Time `gorm:"column:created_at"`
+		APIKey    string     `gorm:"column:api_key"`
+		Method    string     `gorm:"column:method"`
+		ID        int16      `gorm:"column:id;primary_key" json:"id"`
+	}
+)
+
+func (tbl *RouteAccess) TableName() string {
+	return "routes_access"
+}
+
+func (tbl *MethodAccess) TableName() string {
+	return "methods_access"
+}
