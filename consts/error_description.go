2b152974b7acf509bb10df84f35d3bf5bf1bf3e8
Ezra Sitorus <ezra.indra@work.bri.co.id>
2024-06-16 14:23:43 +0700

    A: err_description consts

---
+++ consts/error_description.go
@@ -0,0 +1,9 @@
+package consts
+
+const (
+	ERR_DESC_GENERAL_ISE         = "Internal Server Error"
+	ERR_DESC_GENERAL_BAD_REQUEST = "Bad Request"
+	ERR_DESC_GENERAL_NOT_FOUND   = "Not Found"
+	ERR_DESC_GENERAL_FORBIDDEN   = "Access Unauthorized"
+	ERR_DESC_GENERAL_OK          = "OK"
+)
